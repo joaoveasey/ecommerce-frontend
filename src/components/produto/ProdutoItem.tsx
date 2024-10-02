6a2216c1e32e36cde00cb717ed7e0043500c4d4d
joaoveasey <jovepedro@gmail.com>
2024-10-02 20:36:03 -0300

    feat: implementando novos layouts

--- src/components/produto/ProdutoItem.tsx
+++ src/components/produto/ProdutoItem.tsx
@@ -1,27 +1,39 @@
 'use client'
-import { Produto } from "@/core";
-import Link from "next/link";
-import Image from "next/image";
-import { IconShoppingCartPlus } from "@tabler/icons-react";
+import { Produto } from '@/core'
+import { IconShoppingCartPlus } from '@tabler/icons-react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export interface ProdutoItemProps {
     produto: Produto
 }
 
-export default function ProdutoItem(props: ProdutoItemProps){
+export default function ProdutoItem(props: ProdutoItemProps) {
     const { produto } = props
     return (
-        <Link 
+        <Link
             href={`/produto/${produto.id}`}
             className="flex flex-col bg-violet-dark border border-white/10 rounded-xl relative max-w-[350px]"
         >
-            <div className="h48 w-full relative">
-                <Image src={produto.imagem}alt = 'Imagem do produto' fill className="object-contain"/>
+            <div className="h-48 w-full relative">
+                <Image
+                    src={produto.imagem}
+                    alt="Imagem do produto"
+                    className="object-contain"
+                    fill
+                />
             </div>
-            <div className="flex-1"></div>
-            <div className="flex-1 flex flex-col p-5 gap-3 border-t border-white/10">
-                <span className="text-lg font-semibold ">{props.produto.nome}</span>
-                <span className="text-sm border-b border-dashed self-start">{produto.especificacoes.destaque}</span>
+            <div
+                className="
+                    flex-1 flex flex-col p-5 gap-3 
+                    border-t border-white/10
+                "
+            >
+                <span className="text-lg font-semibold text-white">{props.produto.nome}</span>
+                <span className="text-sm border-b border-dashed self-start text-white">
+                    {produto.especificacoes.destaque}
+                </span>
+                <div className="flex-1"></div>
                 <div className="flex flex-col">
                     <span className="text-sm text-gray-400 line-through">
                         de {props.produto.precoBase}
@@ -30,16 +42,20 @@ export default function ProdutoItem(props: ProdutoItemProps){
                         por {props.produto.precoPromocional}
                     </span>
                 </div>
-                <button className="flex bg-violet-700 rounded-full justify-center items-center hover:border-2 border-emerald-500 h-8 gap-2" 
+                <button
+                    className="
+                        flex justify-center items-center h-8 gap-2
+                        bg-violet-700 hover:border-2 border-emerald-500 rounded-full
+                    "
                     onClick={(e: any) => {
-                    e.preventDefault()
-                    console.log('Adicionado ao carrinho')
-                }}>
+                        e.preventDefault()
+                        console.log('Adicionar ao carrinho')
+                    }}
+                >
                     <IconShoppingCartPlus size={20} />
                     <span>Adicionar</span>
                 </button>
             </div>
-            {props.produto.nome}
         </Link>
-    );
+    )
 }
